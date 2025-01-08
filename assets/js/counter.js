71f5d98a709f71f4ec9389c87a83653694c75695
Tommaso-Ferdinandi <tommaso.ferdinandi@yahoo.com>
2025-01-08 16:18:08 +0100

    Worked on the css, the event listeners and corrected the html file

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -5,6 +5,7 @@ const h1 = document.createElement("h1");
 const display = document.createElement("div");
 const decrease = document.createElement("button");
 const increase = document.createElement("button");
+const text = document.createElement("p");
 const audio = new Audio("assets/audio/retro-select-sound.mp3")
 
 // aggiunta di attrributi, proprietà e appending
@@ -34,3 +35,30 @@ increase.className("button");
 increase.className("increaseCounter");
 increase.textContent("+");
 section.appendChild(increase);
+
+// text
+text.className("text");
+text.textContent("Quando vuoi sono pronto!!")
+section.appendChild(text)
+
+
+// js script in funzione
+let value = 0;
+
+// increase event 
+increase.addEventListener("click", () => {
+  value++;
+  display.textContent = value;
+  text.textcontent = "";
+  audio.play();
+});
+
+// decrease event
+decrease.addEventListener("click", () => {
+  if (value > 0) {
+    value--;
+  }
+  display.textContent = value; 
+  text.textContent = value === 0 ? "Quando vuoi sono pronto!!" : ""; /*check se il counter è tornato a zero*/
+  audio.play();
+});
