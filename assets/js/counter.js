a8d08ecb7aa64f2153ff93be4393d384c2218b76
Tommaso-Ferdinandi <tommaso.ferdinandi@yahoo.com>
2025-01-08 15:15:27 +0100

    changed the js folder structure to avoid modules problem with browser

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,1 +1,36 @@
-import {section, h1, display, decrease, increase, audio} from "./element.js";
+// creazione html element
+
+const section = document.createElement("section");
+const h1 = document.createElement("h1");
+const display = document.createElement("div");
+const decrease = document.createElement("button");
+const increase = document.createElement("button");
+const audio = new Audio("assets/audio/retro-select-sound.mp3")
+
+// aggiunta di attrributi, proprietà e appending
+
+// counter
+section.className("counter");
+document.body.append(section);
+
+// header
+h1.className("header");
+h1.textContent("Divertiti con il Counter!!");
+section.appendChild(h1);
+
+// displayCounter
+display.className("displayCounter");
+display.textContent(0);
+section.appendChild(display);
+
+// decreaseCounter
+decrease.className("button");
+decrease.className("decreaseCounter");
+decrease.textContent("-");
+section.appendChild(decrease);
+
+// increaseCounter
+increase.className("button");
+increase.className("increaseCounter");
+increase.textContent("+");
+section.appendChild(increase);
